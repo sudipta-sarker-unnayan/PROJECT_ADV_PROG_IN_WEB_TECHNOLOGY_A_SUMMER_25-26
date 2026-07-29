71511bff13b68e90804506d78cc07b1108674765
hashameealshahriar <hashameealshahriaraditya@gmail.com>
2026-07-29 17:59:09 +0600

    remove

--- CourseManagementAPI/dist/course/course.service.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CourseService {
-    getAllCourses(): string;
-    getCourseById(id: string): string;
-    createCourse(): string;
-    updateCourse(id: string): string;
-    patchCourse(id: string): string;
-    deleteCourse(id: string): string;
-}
--- CourseManagementRestAPI/dist/course/dto/create-course.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CreateCourseDto {
-    name: string;
-    code: string;
-    instructor: string;
-    credits: number;
-    description?: string;
-}
