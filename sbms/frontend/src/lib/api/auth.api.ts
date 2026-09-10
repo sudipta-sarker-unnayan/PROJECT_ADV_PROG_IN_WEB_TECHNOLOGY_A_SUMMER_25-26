de04a093d08227ed6d8f66f21e3475c384991b8b
Sudipta Sarker Unnayan <22-47656-2@student.aiub.edu>
2026-09-10 22:10:43 +0600

    modify

--- sbms/frontend/src/lib/api/auth.api.ts
+++ sbms/frontend/src/lib/api/auth.api.ts
@@ -1,5 +1,5 @@
 import { api } from "./axios";
-import { LoginPayload, LoginResponse } from "@/lib/types/auth.types";
+import { LoginPayload, LoginResponse } from "../../lib/types/auth.types";
 
 export async function loginRequest(payload: LoginPayload): Promise<LoginResponse> {
   const { data } = await api.post<LoginResponse>("/auth/login", payload);
