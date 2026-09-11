7f574abac30b66dd0ec086a0422157c3d09a3d31
Abdullah Shoab <imshoab883@gmail.com>
2026-09-11 21:41:44 +0600

    Delete sbms/src directory

--- sbms/src/leave/dto/update-status.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEnum } from 'class-validator';
-import { LeaveStatus } from '../entities/leave.entity';
-
-export class UpdateStatusDto{
-
-@IsEnum(LeaveStatus)
-status:LeaveStatus;
-
-}
--- sbms/src/managers/dto/update-manager.dto.ts
+++
@@ -1,6 +0,0 @@
-import { OmitType, PartialType } from '@nestjs/mapped-types';
-import { CreateManagerDto } from './create-manager.dto';
-
-export class UpdateManagerDto extends PartialType(
-    OmitType(CreateManagerDto,['userId'] as const)
-) {}
